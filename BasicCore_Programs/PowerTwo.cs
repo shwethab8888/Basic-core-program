37b558adcb89e7739986208db2ab2a9b31866e74
shwethab8888 <shwethab8888@gmail.com>
2023-02-28 19:16:23 +0530

    Power of Two

---
+++ BasicCore_Programs/PowerTwo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore_Programs
+{
+    public class PowerTwo
+    {
+
+        public static void Power_Two()
+        {
+        
+        
+            static void Main(string[] args)
+            {
+                int n;
+                while (true)
+                {
+                    Console.Write("Enter a power value (0 <= N < 31): ");
+                    if (int.TryParse(Console.ReadLine(), out n) && n >= 0 && n < 31)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Please enter a valid power value.");
+                }
+
+                int i = 0;
+                int powerOfTwo = 1;
+                while (i <= n)
+                {
+                    Console.WriteLine("2^{0} = {1}", i, powerOfTwo);
+                    i++;
+                    powerOfTwo *= 2;
+                }
+            }
+        }
+    
+    }
+}
--- BasicCore_Programs/Program.cs
+++ BasicCore_Programs/Program.cs
@@ -2,12 +2,12 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             //Console.WriteLine("Welcome to Basic Core Programs");
             //FlipCoin.Flip_Coin();
-            LeapYear.Leap_Year();
-
+            //LeapYear.Leap_Year();
+            PowerTwo.Power_Two();
         }
 
     }
