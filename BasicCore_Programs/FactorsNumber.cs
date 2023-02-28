2c1194a4362f5b43f3ca45d21b60bac12c552e34
shwethab8888 <shwethab8888@gmail.com>
2023-02-28 19:36:22 +0530

    factors

---
+++ BasicCore_Programs/FactorsNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore_Programs
+{
+    public class FactorsNumber
+    {
+        public static void Factors_Number()
+        {
+            static void Main()
+            {
+                Console.Write("Enter a positive integer N: ");
+                int n = int.Parse(Console.ReadLine());
+
+                Console.Write("The prime factors of {0} are: ", n);
+                for (int i = 2; i * i <= n; i++)
+                {
+                    while (n % i == 0)
+                    {
+                        Console.Write(i + " ");
+                        n /= i;
+                    }
+                }
+
+                if (n > 1)
+                    Console.Write(n);
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- BasicCore_Programs/Program.cs
+++ BasicCore_Programs/Program.cs
@@ -2,13 +2,16 @@
 {
     public class Program
     {
+        private static object factorsNumber;
+
         public static void Main(string[] args)
         {
             //Console.WriteLine("Welcome to Basic Core Programs");
             //FlipCoin.Flip_Coin();
             //LeapYear.Leap_Year();
             //PowerTwo.Power_Two();
-            HarmonicNumber.Harmonic_Number();
+            // HarmonicNumber.Harmonic_Number();
+            factorsNumber.factors_Number();
         }
 
     }
