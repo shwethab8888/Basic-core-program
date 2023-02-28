a36cd95cd4852555bbaf6eedc1bdfc1c8f5b150e
shwethab8888 <shwethab8888@gmail.com>
2023-02-28 18:46:44 +0530

    Flip Coin

---
+++ BasicCore_Programs/FlipCoin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCore_Programs
+{
+    public class FlipCoin
+    {
+        class Program
+        {
+
+
+            public static void Flip_Coin()
+            {
+
+
+                static void Main(string[] args)
+                {
+                    int numFlips;
+                    while (true)
+                    {
+                        Console.Write("Enter the number of times to flip the coin: ");
+                        if (int.TryParse(Console.ReadLine(), out numFlips) && numFlips > 0)
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Please enter a positive integer.");
+                    }
+
+                    Random rand = new Random();
+                    int heads = 0;
+                    int tails = 0;
+                    for (int i = 0; i < numFlips; i++)
+                    {
+                        if (rand.NextDouble() < 0.5)
+                        {
+                            tails++;
+                        }
+                        else
+                        {
+                            heads++;
+                        }
+                    }
+
+                    double headsPercent = (double)heads / numFlips * 100;
+                    double tailsPercent = (double)tails / numFlips * 100;
+                    Console.WriteLine("Percentage of Heads: {0:F2}%", headsPercent);
+                    Console.WriteLine("Percentage of Tails: {0:F2}%", tailsPercent);
+                }
+
+            }
+        }
+    }
+}
+
+
---
+++ BasicCore_Programs/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BasicCore_Programs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to Basic Core Programs");
+            FlipCoin.Flip_Coin(); 
+        }
+
+    }
+}
